0c74ddaddc6d52a7408c8e3fe2305540eab877b3
JIINHEO <gjwldls123@gmail.com>
2021-11-25 16:15:21 +0900

    first commit

---
+++ ios-netflix-style-sample-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-netflix-style-sample-app
+//
+//  Created by 허지인 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
