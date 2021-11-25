d315bc7a47d510dd7db8f35832428d72d240b474
JIINHEO <gjwldls123@gmail.com>
2021-11-25 16:45:23 +0900

    Feat: add Snapkit

---
+++ ios-netflix-style-sample-app/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  ios-netflix-style-sample-app
+//
+//  Created by 허지인 on 2021/11/25.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+    
+}
