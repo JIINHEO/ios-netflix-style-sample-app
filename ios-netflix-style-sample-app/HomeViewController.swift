dbdf643a98e03686a69f451e7964a6a8a777b869
JIINHEO <gjwldls123@gmail.com>
2021-11-29 21:12:37 +0900

    [#3] Feat: 리스트 구현

--- ios-netflix-style-sample-app/HomeViewController.swift
+++ ios-netflix-style-sample-app/HomeViewController.swift
@@ -11,6 +11,7 @@ import SwiftUI
 class HomeViewController: UICollectionViewController {
     
     var contents: [Content] = []
+    var mainItem: Item?
     
     override func viewDidLoad() {
         
@@ -25,10 +26,12 @@ class HomeViewController: UICollectionViewController {
          
         //Data설정, 가져오기
         contents = getContent()
+        mainItem = contents.first?.contentItem.randomElement()
         
         //CollectionView Item(Cell) 설정
         collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
         collectionView.register(ContentCollectionViewRankCell.self, forCellWithReuseIdentifier: "ContentCollectionViewRankCell")
+        collectionView.register(ContentCollectionViewMainCell.self, forCellWithReuseIdentifier: "ContentCollectionViewMainCell")
         collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
         
         collectionView.collectionViewLayout = layout()
@@ -54,6 +57,8 @@ class HomeViewController: UICollectionViewController {
                 return self.createLargeTypeSection()
             case .rank:
                 return self.createRankTypeSection()
+            case .main:
+                return self.createMainTypeSection()
             default:
                 return nil
             }
@@ -125,8 +130,6 @@ class HomeViewController: UICollectionViewController {
         return section
     }
     
-    
-    
     //sectionHeader layout 설정
     private func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem{
         //Section Header Size
@@ -136,23 +139,34 @@ class HomeViewController: UICollectionViewController {
         let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSectionHeaderSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
         return sectionHeader
     }
+    
+    //Main Section Layout
+    private func createMainTypeSection() -> NSCollectionLayoutSection {
+        //item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        //group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(450))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        //section
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 20, trailing: 0)
+        return section
+    }
 }
 
 //MARK: UICollectionVIew DAtaSoure, Delegate
 extension HomeViewController {
     //섹션당 보여질 셀의 개수
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if contents[section].sectionType == .basic
-            || contents[section].sectionType == .large
-            || contents[section].sectionType == .rank {
-            switch section {
-            case 0:
-                return 1
-            default:
-                return contents[section].contentItem.count
-            }
+        switch section {
+        case 0:
+            return 1
+        default:
+            return contents[section].contentItem.count
         }
-        return 0
     }
     
     // 콜렉션뷰 셀 설정
@@ -170,6 +184,11 @@ extension HomeViewController {
             cell.rankLabel.text = String(describing: indexPath.row + 1 )
             return cell
             
+        case .main:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewMainCell", for: indexPath) as? ContentCollectionViewMainCell else { return UICollectionViewCell()}
+            cell.imageView.image = mainItem?.image
+            cell.descriptionLabel.text = mainItem?.description
+            return cell
         default:
             return UICollectionViewCell()
         }
@@ -199,7 +218,7 @@ extension HomeViewController {
 }
 
 ////swiftUI를 활용한 미리보기
-//#if DEBUG
+#if DEBUG
 struct HomeViewController_Preview: PreviewProvider{
 
     static var previews : some View {
@@ -217,4 +236,4 @@ struct HomeViewController_Preview: PreviewProvider{
         typealias UIViewControllerType = UIViewController
     }
 }
-//#endif
+#endif
---
+++ ios-netflix-style-sample-app/UIButton.swift
@@ -0,0 +1,19 @@
+//
+//  UIButton.swift
+//  ios-netflix-style-sample-app
+//
+//  Created by 허지인 on 2021/11/29.
+//
+
+import UIKit
+
+extension UIButton {
+    func adjustVerticalLayout(_ spacing: CGFloat = 0) {
+        let imageSize = self.imageView?.frame.size ?? .zero
+        let titleLabelSize = self.titleLabel?.frame.size ?? .zero
+        
+        self.titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageSize.width, bottom: -(imageSize.height + spacing), right: 0)
+        self.imageEdgeInsets = UIEdgeInsets(top: -(titleLabelSize.height + spacing), left: 0, bottom: 0, right: -titleLabelSize.width)
+        
+    }
+}
